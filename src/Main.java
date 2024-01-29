302b00984580f8b210779e77abafbba5040ece08
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-29 12:16:42 +0300

    Пул Integer весьма ограничен, 15 == 15 --> true, а 150 == 150 --> false

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int a = 15;
         Integer aa = 15;
+        Integer bb = 15;
 
+        System.out.println(aa == bb); //true
+
+        Integer cc = 150;
+        Integer dd = 150;
+
+        System.out.println(cc == dd); //false
     }
 }
