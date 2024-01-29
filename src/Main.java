5f520b1e0d48ebc17dbab877541514c0fa40c5f7
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-29 12:55:23 +0300

    В этом коде int lastNum используется для хранения последней цифры введенной строки. Вот как это работает:
    String input = new Scanner(System.in).nextLine(); - эта строка считывает ввод пользователя и сохраняет его в переменной input.
    input.charAt(input.length()-1) - эта часть кода получает последний символ введенной строки. Метод length() возвращает длину строки, и когда мы вычитаем 1, мы получаем индекс последнего символа (поскольку индексация начинается с 0).
    String.valueOf(input.charAt(input.length()-1)) - здесь мы преобразуем последний символ в строку. Это необходимо, потому что метод Integer.parseInt() принимает строку в качестве аргумента.
    int lastNum = Integer.parseInt(String.valueOf(input.charAt(input.length()-1))); - в этой строке мы преобразуем строку, представляющую последний символ, в целое число и сохраняем его в переменной lastNum.
    System.out.println(lastNum); - здесь мы выводим значение lastNum в консоль.
    Таким образом, если пользователь вводит строку “12345”, то lastNum будет равно 5, и это число будет выведено в консоль. Если последний символ введенной строки не является цифрой, то будет выброшено исключение NumberFormatException.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "123";
-        Integer i = 123;
+    String input = new Scanner(System.in).nextLine();
 
-        String test1 = String.valueOf(i);
-        System.out.println(test1 + 1);
+    int lastNum = Integer.parseInt(String.valueOf(input.charAt(input.length()-1)));
+        System.out.println(lastNum);
     }
 }
