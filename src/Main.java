4b79ed8eab185f751d96c9b34f1ce753843ee997
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-29 11:31:33 +0300

    Обучение стекуи и хипу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Human vasya = new Human("Vasya", 20);
+        Human petya = new Human(vasya.getName(), vasya.getAge());
+
+        vasya.setName("Timur");
+
+        System.out.println(vasya.getName());
+        System.out.println(petya.getName());
+
     }
 }
